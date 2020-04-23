48a67f68d23f7880b5a8adccdb2168b5619161db
Andrew Venson <vensoneandrew@gmail.com>
2020-04-23 01:26:39 -0500

    Updates

---
+++ precache-manifest.6fb02cd4c8a6c1e337bdcd4b203b14d3.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c922c7ce5cac24732cf6c700de331125",
+    "url": "/drews_portfolio/index.html"
+  },
+  {
+    "revision": "7c4527bf982f03b68437",
+    "url": "/drews_portfolio/static/css/main.07ebec76.chunk.css"
+  },
+  {
+    "revision": "7199968af81f90c13902",
+    "url": "/drews_portfolio/static/js/2.a5ce7724.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/drews_portfolio/static/js/2.a5ce7724.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7c4527bf982f03b68437",
+    "url": "/drews_portfolio/static/js/main.c2fae7d5.chunk.js"
+  },
+  {
+    "revision": "df4a241b495c65afde9f",
+    "url": "/drews_portfolio/static/js/runtime-main.e68db22c.js"
+  },
+  {
+    "revision": "af25e683b9a66ddf279939e77d47ad3a",
+    "url": "/drews_portfolio/static/media/mysql.af25e683.png"
+  },
+  {
+    "revision": "1e4391bbdec410d74dd257c4f7019b1f",
+    "url": "/drews_portfolio/static/media/profilePic.1e4391bb.png"
+  },
+  {
+    "revision": "cb5e0d8ca6a5c0e37a0ab90911b8f949",
+    "url": "/drews_portfolio/static/media/pycdv.cb5e0d8c.PNG"
+  },
+  {
+    "revision": "d21e6f1cb4a71af4fa79ccaf9e49c521",
+    "url": "/drews_portfolio/static/media/tbc.d21e6f1c.jpg"
+  }
+]);
